1748ef435496a955e046619d7daba36953959791
Elena <mymoskvina@gmail.com>
2020-10-15 10:15:37 +0300

    Radio code commit

---
+++ src/main/java/ru/netology/radio/RadioService.java
@@ -0,0 +1,136 @@
+package ru.netology.radio;
+
+public class RadioService {
+    private String name;
+    private int currentStation;
+    private int currentVolume;
+    private int maxVolume;
+    private int minVolume;
+    private int firstStation;
+    private int lastStation;
+    private int nextStation;
+    private int previousStation;
+    private boolean on;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (this.currentStation > lastStation) {
+            this.currentStation = firstStation;
+        }
+        if (this.currentStation < firstStation) {
+            this.currentStation = lastStation;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+    //установка текущей громкости
+    public void setCurrentVolume(int currentVolume) {
+        if (this.currentVolume > maxVolume) {
+            return;
+        }
+        if (this.currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void increaseCurrentVolume() {
+        if (currentVolume < maxVolume) {
+            this.currentVolume++;
+        }
+    }
+
+    public void decreaseCurrentVolume () {
+        if (currentVolume > minVolume) {
+            this.currentVolume--;
+        }
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void goToNextStation() {
+        if (currentStation < lastStation) {
+            this.currentStation++;
+        }
+        else {
+            this.currentStation = firstStation;
+        }
+    }
+
+    public void goToPreviousStation () {
+        if (currentStation > firstStation) {
+            this.currentStation--;
+        }
+        else {
+            this.currentStation = lastStation;
+        }
+    }
+
+    public int getFirstStation() {
+        return firstStation;
+    }
+
+    public void setFirstStation(int firstStation) {
+        this.firstStation = firstStation;
+    }
+
+    public int getLastStation() {
+        return lastStation;
+    }
+
+    public void setLastStation(int lastStation) {
+        this.lastStation = lastStation;
+    }
+
+    public int getNextStation() {
+        return nextStation;
+    }
+
+    public void setNextStation(int nextStation) {
+        this.nextStation = nextStation;
+    }
+
+    public int getPreviousStation() {
+        return previousStation;
+    }
+
+    public void setPreviousStation(int previousStation) {
+        this.previousStation = previousStation;
+    }
+}
---
+++ src/test/java/ru/netology/radio/RadioServiceTest.java
@@ -0,0 +1,267 @@
+package ru.netology.radio;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioServiceTest {
+    // проверяем имя
+    @Test
+    void ShouldGetAndSet() {
+        RadioService radio = new RadioService();
+        String expected = "Super Radio";
+        assertNull(radio.getName());
+        radio.setName(expected);
+        assertEquals(expected, radio.getName());
+    }
+    //преключаем на предыдущю
+    @Test
+    public void GoToPreviousStation() {
+        RadioService radio = new RadioService();
+        radio.setFirstStation(0);
+        radio.setLastStation(9);
+        int currentStation = 5;
+        int expected = 4;
+        radio.setCurrentStation(currentStation);
+        radio.goToPreviousStation();
+        assertEquals(expected, radio.getCurrentStation());
+    }
+    // переключаем на предыдущую, если на 0 значении
+    @Test
+    public void GoToPreviousStationIfZero() {
+        RadioService radio = new RadioService();
+        radio.setFirstStation(0);
+        radio.setLastStation(9);
+        int currentStation = 0;
+        int expected = 9;
+        radio.setCurrentStation(currentStation);
+        radio.goToPreviousStation();
+        assertEquals(expected, radio.getCurrentStation());
+    }
+    // переключаем на предыдущую, если на максимальном значении
+    @Test
+    public void GoToPreviousStationIfMax() {
+        RadioService radio = new RadioService();
+        radio.setFirstStation(0);
+        radio.setLastStation(9);
+        int currentStation = 9;
+        int expected = 8;
+        radio.setCurrentStation(currentStation);
+        radio.goToPreviousStation();
+        assertEquals(expected, radio.getCurrentStation());
+    }
+    // переключаем на следующую
+    @Test
+    public void GoToNextStation() {
+        RadioService radio = new RadioService();
+        radio.setFirstStation(0);
+        radio.setLastStation(9);
+        int currentStation = 5;
+        int expected = 6;
+        radio.setCurrentStation(currentStation);
+        radio.goToNextStation();
+        assertEquals(expected, radio.getCurrentStation());
+    }
+    // переключаем на следующую, если на 0 значении
+    @Test
+    public void GoToNextStationIfSelectedZeroStation() {
+        RadioService radio = new RadioService();
+        radio.setFirstStation(0);
+        radio.setLastStation(9);
+        int currentStation = 0;
+        int expected = 1;
+        radio.setCurrentStation(currentStation);
+        radio.goToNextStation();
+        assertEquals(expected, radio.getCurrentStation());
+    }
+    // переключаем на следующую, если на максимальном значении
+    @Test
+    public void GoToNextStationIfMax() {
+        RadioService radio = new RadioService();
+        radio.setFirstStation(0);
+        radio.setLastStation(9);
+        int currentStation = 9;
+        int expected = 0;
+        radio.setCurrentStation(currentStation);
+        radio.goToNextStation();
+        assertEquals(expected, radio.getCurrentStation());
+    }
+    // набираем станцию на пульте
+    @Test
+    void setCurrentStation() {
+        RadioService radio = new RadioService();
+        int currentStation = 3;
+        int expected = 3;
+        radio.setCurrentStation(currentStation);
+        assertEquals(expected, radio.getCurrentStation());
+    }
+    //набираем 0 станцию на пульте
+    @Test
+    void setCurrentStationZero() {
+        RadioService radio = new RadioService();
+        int currentStation = 0;
+        int expected = 0;
+        radio.setCurrentStation(currentStation);
+        assertEquals(expected, radio.getCurrentStation());
+    }
+    // набираем максимальную станцию на пульте
+    @Test
+    void setCurrentStationMax() {
+        RadioService radio = new RadioService();
+        int currentStation = 9;
+        int expected = 9;
+        radio.setCurrentStation(currentStation);
+        assertEquals(expected, radio.getCurrentStation());
+    }
+
+    //уменьшаем громкость на 1
+    @Test
+    public void DecreaseVolume() {
+        RadioService radio = new RadioService();
+        radio.setMinVolume(0);
+        radio.setMaxVolume(10);
+        int currentVolume = 5;
+        int expected = 4;
+        radio.setCurrentVolume(currentVolume);
+        radio.decreaseCurrentVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    //уменьшаем громкость на 1 если стоит 0
+    @Test
+    public void DecreaseVolumeIfZero() {
+        RadioService radio = new RadioService();
+        radio.setMinVolume(0);
+        radio.setMaxVolume(10);
+        int currentVolume = 0;
+        int expected = 0;
+        radio.setCurrentVolume(currentVolume);
+        radio.decreaseCurrentVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    //уменьшаем громкость на 1 если стоит 10
+    @Test
+    public void DecreaseVolumeIfMax() {
+        RadioService radio = new RadioService();
+        radio.setMinVolume(0);
+        radio.setMaxVolume(10);
+        int currentVolume = 10;
+        int expected = 9;
+        radio.setCurrentVolume(currentVolume);
+        radio.decreaseCurrentVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    //увеличиваем громкость на 1
+    @Test
+    public void IncreaseVolume() {
+        RadioService radio = new RadioService();
+        radio.setMinVolume(0);
+        radio.setMaxVolume(10);
+        int currentVolume = 5;
+        int expected = 6;
+        radio.setCurrentVolume(currentVolume);
+        radio.increaseCurrentVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    //увеличиваем громкость на 1 если 0
+    @Test
+    public void IncreaseVolumeIfZero() {
+        RadioService radio = new RadioService();
+        radio.setMinVolume(0);
+        radio.setMaxVolume(10);
+        int currentVolume = 0;
+        int expected = 1;
+        radio.setCurrentVolume(currentVolume);
+        radio.increaseCurrentVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    //увеличиваем громкость на 1 если максимальная громкость
+    @Test
+    public void IncreaseVolumeIfMax() {
+        RadioService radio = new RadioService();
+        radio.setMinVolume(0);
+        radio.setMaxVolume(10);
+        int currentVolume = 10;
+        int expected = 10;
+        radio.setCurrentVolume(currentVolume);
+        radio.increaseCurrentVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    // Тесты для покрытия JaCoCo
+    @Test
+    void CheckGetAndSetCurrentStation() {
+        RadioService radio = new RadioService();
+        radio.setCurrentStation(8);
+        int expected = 8;
+        radio.setCurrentStation(expected);
+        assertEquals(expected, radio.getCurrentStation());
+    }
+    @Test
+    void CheckGetAndSetPreviousStation() {
+        RadioService radio = new RadioService();
+        radio.setPreviousStation(8);
+        int expected = 8;
+        radio.setPreviousStation(expected);
+        assertEquals(expected, radio.getPreviousStation());
+    }
+    @Test
+    void CheckGetAndSetNextStation() {
+        RadioService radio = new RadioService();
+        radio.setNextStation(8);
+        int expected = 8;
+        radio.setNextStation(expected);
+        assertEquals(expected, radio.getNextStation());
+    }
+    @Test
+    void CheckGetAndSetLastStation() {
+        RadioService radio = new RadioService();
+        radio.setLastStation(9);
+        int expected = 9;
+        radio.setLastStation(expected);
+        assertEquals(expected, radio.getLastStation());
+    }
+    @Test
+    void CheckGetAndSetFirstStation() {
+        RadioService radio = new RadioService();
+        radio.setFirstStation(1);
+        int expected = 1;
+        radio.setFirstStation(expected);
+        assertEquals(expected, radio.getFirstStation());
+    }
+    @Test
+    void CheckGetAndSetCurrentVolume() {
+        RadioService radio = new RadioService();
+        radio.setCurrentVolume(8);
+        int expected = 8;
+        radio.setCurrentVolume(expected);
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    @Test
+    void CheckGetAndSetMaxVolume() {
+        RadioService radio = new RadioService();
+        radio.setMaxVolume(10);
+        int expected = 10;
+        radio.setMaxVolume(expected);
+        assertEquals(expected, radio.getMaxVolume());
+    }
+    @Test
+    void CheckGetAndSetMinVolume() {
+        RadioService radio = new RadioService();
+        radio.setMinVolume(0);
+        int expected = 0;
+        radio.setMinVolume(expected);
+        assertEquals(expected, radio.getMinVolume());
+    }
+    @Test
+    void ShouldGetAndSetOn() {
+        RadioService radio = new RadioService();
+        radio.setOn(false);
+        boolean expected = false;
+        assertEquals(expected, radio.isOn());
+    }
+    @Test
+    void CheckGetAndSetCurrentStationForCoverage() {
+        RadioService radio = new RadioService();
+        radio.setCurrentStation(8);
+        int expected = 8;
+        radio.setCurrentStation(expected);
+        assertEquals(expected, radio.getCurrentStation());
+    }
+}
